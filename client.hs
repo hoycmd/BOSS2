41d298719233fc2cec26961f6b51b9ebdeafd077
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-03-04 22:52:50 +0700

    Update client.hs

--- client.hs
+++ client.hs
@@ -1,4 +1,17 @@
 var admin = ["2827CD16AE7CC982"];
-player.Main.Value = true;
-player.MainInfinity.Value = true;
-player.Mapee
+player.function.Main.Value = true;
+player.function.MainInfinity.Value = true;
+player.function.Melee.Value = true;
+player.function.ExpIosive.Value = true;
+player.function.ExplosiveInfinity.Value = true;
+player.function.Build.Value = true;
+player.function.BuildInfinity.Value = true;
+
+
+
+
+
+
+
+
+
