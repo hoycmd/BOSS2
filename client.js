49d904a573971ca0b512c4315dbc697727525c3e
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-03-05 00:00:05 +0700

    Update client.js

--- client.js
+++ client.js
@@ -39,6 +39,122 @@ Ui.GetContext().TeamProp2.Value = { Team: "Blue", Prop: "Des" };
 Teams.Get("Blue").Properties.Get("Des").Value = des;  
 Ui.GetContext().TeamProp1.Value = { Team: "Red", Prop: "Des" };
 
+Damage.GetContext().FriendlyFire.Value = true;
+
+// ��������� ��������� �������� �������
+Damage.GetContext().DamageOut.Value = GameMode.Parameters.GetBool("Damage");
+BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
+BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
+Build.GetContext().FloodFill.Value =  GameMode.Parameters.GetBool("FloodFill");
+Build.GetContext().FillQuad.Value =  GameMode.Parameters.GetBool("FillQuad");
+Build.GetContext().RemoveQuad.Value =  GameMode.Parameters.GetBool("RemoveQuad");
+Build.GetContext().FlyEnable.Value =  GameMode.Parameters.GetBool("Fly");
+// задаем что выводить в лидербордах
+LeaderBoard.PlayerLeaderBoardValues = [
+ {
+  Value: "Kills",
+  DisplayName: "Убийства",
+  ShortDisplayName: "Убийства"
+ },
+ {
+  Value: "Deaths",
+  DisplayName: "Смерти",
+  ShortDisplayName: "Смерти"
+ },
+{
+  Value: "Status",
+  DisplayName: "Статус",
+  ShortDisplayName: "Статус"
+ },
+ {
+  Value: "Scores",
+  DisplayName: "Очки",
+  ShortDisplayName: "Очки"
+ }
+];
+LeaderBoard.TeamLeaderBoardValue = {
+ Value: "Deaths",
+ DisplayName: "Statistics\Deaths",
+ ShortDisplayName: "Statistics\Deaths"
+};
+
+
+
+
+// ������ ��������� ������ ��� �����
+BreackGraph.BreackAll = true;
+// ���������� ���������� ������
+Ui.GetContext().QuadsCount.Value = true;
+// ��� ������������ �����
+Build.GetContext().Pipette.Value = true;
+Build.GetContext().BalkLenChange.Value = true;
+Build.GetContext().SetSkyEnable.Value = true;
+Build.GetContext().GenMapEnable.Value = true;
+Build.GetContext().ChangeCameraPointsEnable.Value = true;
+Build.GetContext().QuadChangeEnable.Value = true;
+Build.GetContext().BuildModeEnable.Value = true;
+Build.GetContext().CollapseChangeEnable.Value = true;
+Build.GetContext().RenameMapEnable.Value = true;
+Build.GetContext().ChangeMapAuthorsEnable.Value = true;
+Build.GetContext().LoadMapEnable.Value = true;
+Build.GetContext().ChangeSpawnsEnable.Value = true;
+
+// ��������� ����
+Properties.GetContext().GameModeName.Value = "GameModes/Peace";
+// ������� �������
+red = GameMode.Parameters.GetBool("RedTeam");
+blue = GameMode.Parameters.GetBool("BlueTeam");
+if (red || !red && !blue) {
+ Teams.Add("Red", "<color=red>АДМИНЫ</a>", { r
+: 80 });
+ Teams.Get("Red").Spawns.SpawnPointsGroups.Add
+(1);
+}
+
+// ������ ���������  
+Ui.GetContext().Hint.Value = "прив"; 
+  
+des = "@Vanhek34";  
+sed = "@Vanhek34";  
+Teams.Get("Blue").Properties.Get("Des").Value = des;  
+Ui.GetContext().TeamProp1.Value = { Team: "Red", Prop: "sed" };  
+Teams.Get("Red").Properties.Get("sed").Value = sed; 
+ 
+Ui.GetContext().TeamProp2.Value = { Team: "Blue", Prop: "Des" };
+var maxDeaths = Players.MaxCount * 6;
+Teams.Get("Red").Properties.Get("Deaths").Value = maxDeaths;
+Teams.Get("Blue").Properties.Get("Deaths").Value = maxDeaths;
+// задаем что выводить в лидербордах
+LeaderBoard.PlayerLeaderBoardValues = [
+ {
+  Value: "Status",
+  DisplayName: "<color=orange>Status</a>",
+  ShortDisplayName: "<color=orange>Status</a>"
+ },
+ {
+  Value: "Scores",
+  DisplayName: "<color=pink>money</a>",
+  ShortDisplayName: "<color=pink>money</a>"
+ },
+ {
+  Value: "Spawns",
+  DisplayName: "Donat money",
+  ShortDisplayName: "Donat money"
+ },
+ {
+
+  Value: "Kills",
+  DisplayName: "color=yellow>пропуск</a>",
+  ShortDisplayName: "<color=yellow>пропуск</a>", }
+
+
+
+];
+LeaderBoard.TeamLeaderBoardValue = {
+ Value: "Deaths",
+ DisplayName: "<color=gooboy>death</a>",
+ ShortDisplayName: "<color=gooboy>death</a>",
+
 
 // создаем команды
 red = GameMode.Parameters.GetBool("RedTeam");
@@ -61,7 +177,129 @@ if (red || !red && !blue) {
   Teams.Get("Blue").Inventory.ExpIosive.Value = false;
   Teams.Get("Blue").Inventory.Build.Value = false;
 
-  // задаем что выводить в лидербордах
+  Damage.GetContext().FriendlyFire.Value = true;
+
+// ��������� ��������� �������� �������
+Damage.GetContext().DamageOut.Value = GameMode.Parameters.GetBool("Damage");
+BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
+BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
+Build.GetContext().FloodFill.Value =  GameMode.Parameters.GetBool("FloodFill");
+Build.GetContext().FillQuad.Value =  GameMode.Parameters.GetBool("FillQuad");
+Build.GetContext().RemoveQuad.Value =  GameMode.Parameters.GetBool("RemoveQuad");
+Build.GetContext().FlyEnable.Value =  GameMode.Parameters.GetBool("Fly");
+// задаем что выводить в лидербордах
+LeaderBoard.PlayerLeaderBoardValues = [
+ {
+  Value: "Kills",
+  DisplayName: "Убийства",
+  ShortDisplayName: "Убийства"
+ },
+ {
+  Value: "Deaths",
+  DisplayName: "Смерти",
+  ShortDisplayName: "Смерти"
+ },
+{
+  Value: "Status",
+  DisplayName: "Статус",
+  ShortDisplayName: "Статус"
+ },
+ {
+  Value: "Scores",
+  DisplayName: "Очки",
+  ShortDisplayName: "Очки"
+ }
+];
+LeaderBoard.TeamLeaderBoardValue = {
+ Value: "Deaths",
+ DisplayName: "Statistics\Deaths",
+ ShortDisplayName: "Statistics\Deaths"
+};
+
+
+
+
+// ������ ��������� ������ ��� �����
+BreackGraph.BreackAll = true;
+// ���������� ���������� ������
+Ui.GetContext().QuadsCount.Value = true;
+// ��� ������������ �����
+Build.GetContext().Pipette.Value = true;
+Build.GetContext().BalkLenChange.Value = true;
+Build.GetContext().SetSkyEnable.Value = true;
+Build.GetContext().GenMapEnable.Value = true;
+Build.GetContext().ChangeCameraPointsEnable.Value = true;
+Build.GetContext().QuadChangeEnable.Value = true;
+Build.GetContext().BuildModeEnable.Value = true;
+Build.GetContext().CollapseChangeEnable.Value = true;
+Build.GetContext().RenameMapEnable.Value = true;
+Build.GetContext().ChangeMapAuthorsEnable.Value = true;
+Build.GetContext().LoadMapEnable.Value = true;
+Build.GetContext().ChangeSpawnsEnable.Value = true;
+
+// ��������� ����
+Properties.GetContext().GameModeName.Value = "GameModes/Peace";
+// ������� �������
+red = GameMode.Parameters.GetBool("RedTeam");
+blue = GameMode.Parameters.GetBool("BlueTeam");
+if (red || !red && !blue) {
+ Teams.Add("Red", "<color=red>АДМИНЫ</a>", { r
+: 80 });
+ Teams.Get("Red").Spawns.SpawnPointsGroups.Add
+(1);
+}
+
+// ������ ���������  
+Ui.GetContext().Hint.Value = "прив"; 
+  
+des = "@Vanhek34";  
+sed = "@Vanhek34";  
+Teams.Get("Blue").Properties.Get("Des").Value = des;  
+Ui.GetContext().TeamProp1.Value = { Team: "Red", Prop: "sed" };  
+Teams.Get("Red").Properties.Get("sed").Value = sed; 
+ 
+Ui.GetContext().TeamProp2.Value = { Team: "Blue", Prop: "Des" };
+var maxDeaths = Players.MaxCount * 6;
+Teams.Get("Red").Properties.Get("Deaths").Value = maxDeaths;
+Teams.Get("Blue").Properties.Get("Deaths").Value = maxDeaths;
+  
+// задаем что выводить в лидербордах
+LeaderBoard.PlayerLeaderBoardValues = [
+ {
+  Value: "Status",
+  DisplayName: "<color=orange>Status</a>",
+  ShortDisplayName: "<color=orange>Status</a>"
+ },
+ {
+  Value: "Scores",
+  DisplayName: "<color=pink>money</a>",
+  ShortDisplayName: "<color=pink>money</a>"
+ },
+ {
+  Value: "Spawns",
+  DisplayName: "Donat money",
+  ShortDisplayName: "Donat money"
+ },
+ {
+
+  Value: "Kills",
+  DisplayName: "color=yellow>пропуск</a>",
+  ShortDisplayName: "<color=yellow>пропуск</a>", }
+
+
+
+];
+LeaderBoard.TeamLeaderBoardValue = {
+ Value: "Deaths",
+ DisplayName: "<color=gooboy>death</a>",
+ ShortDisplayName: "<color=gooboy>death</a>"
+};
+
+
+
+
+
+
   
    
 
